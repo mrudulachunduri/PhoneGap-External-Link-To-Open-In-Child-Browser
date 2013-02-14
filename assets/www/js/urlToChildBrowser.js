b92a0cbebf65c70664f8a839b3038dd8705ab2bd
rajiv <madhu@webileapps.com>
2013-02-14 12:48:05 +0100

    Add the real stuff

---
+++ assets/www/js/urlToChildBrowser.js
@@ -0,0 +1,42 @@
+var currIndex = 0;
+var newHtmlContent = "";
+
+function urlToChildBrowser(oldHtmlContent)
+{
+	oldIndex = 0;
+	currIndex = 0;
+	newHtmlContent = "";
+	tempHtmlContent = ""	
+	urlString= "";
+	
+	while(oldHtmlContent.indexOf("href=") != -1)
+	{		
+		currIndex = oldHtmlContent.indexOf('href="');
+		newHtmlContent = newHtmlContent + oldHtmlContent.substring(0,currIndex);
+		oldHtmlContent = oldHtmlContent.substring(currIndex+6);
+		currIndex = oldHtmlContent.indexOf('"');
+		urlString = oldHtmlContent.substring(0,currIndex+1);
+		//newHtmlContent = newHtmlContent + "#";
+		newHtmlContent = newHtmlContent.insert(newHtmlContent.length+1," class='blue-text' onclick='window.plugins.childBrowser.showWebPage(\""+urlString+", { showLocationBar: false });'");
+		oldHtmlContent = oldHtmlContent.substring(urlString.length);
+	}
+	if(oldHtmlContent != "")
+	{
+		newHtmlContent = newHtmlContent + oldHtmlContent;
+	}
+	console.log(newHtmlContent);
+	if(newHtmlContent == "")
+	{
+		return oldHtmlContent;
+	}
+	else
+		return newHtmlContent;
+	
+}
+
+String.prototype.insert = function (index, string) {
+  if (index > 0)
+    return this.substring(0, index) + string + this.substring(index, this.length);
+  else
+    return string + this;
+};
